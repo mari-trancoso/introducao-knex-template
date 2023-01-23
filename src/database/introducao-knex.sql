d04a1ab8734b31fc0d7c2ee3a59b2978e18e6555
mari-trancoso <mnegraotrancoso@gmail.com>
2023-01-23 14:07:26 -0300

    introdução ao knex

--- src/database/introducao-knex.sql
+++ src/database/introducao-knex.sql
@@ -1,9 +1,14 @@
+-- Active: 1674477642267@@127.0.0.1@3306
 
 -- Tabelas já foram criadas
 CREATE TABLE bands (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL
 );
+
+DROP TABLE bands;
+
+DROP TABLES songs;
 
 CREATE TABLE songs (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { db } from './database/knex'
 
 const app = express()
 
@@ -25,5 +26,129 @@ app.get("/ping", async (req: Request, res: Response) => {
         } else {
             res.send("Erro inesperado")
         }
+    }
+})
+
+app.get("/bands", async (req:Request, res:Response) => {
+    try {
+        const result = await db.raw('SELECT * FROM bands')
+
+        res.status(200).send(result)
+    } catch(error){
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+})
+
+app.post('/bands', async(req:Request, res:Response) => {
+    try{
+        const name = req.body.name
+        const id = req.body.id
+
+        if(!id || !name){
+            res.status(400)
+            throw new Error("Dado inválido")
+        }
+        if(typeof id !== "string"){
+            res.status(400)
+            throw new Error("Id deve ser um texto")
+        }
+        if(typeof name !== "string"){
+            res.status(400)
+            throw new Error("Name deve ser um texto")
+        }
+
+        await db.raw(`
+            INSERT INTO bands (id, name)
+            VALUES("${id}", "${name}")`)
+        
+        res.status(200).send('Banda cadastrada com sucesso!')
+
+    }catch(error){
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+})
+
+app.put('/bands/:id', async(req:Request, res:Response) => {
+    try{
+        const id = req.params.id
+
+        const newId = req.body.id
+        const newName = req.body.name
+
+        if(newId !== undefined){
+            if(typeof newId !== "string"){
+                res.status(400)
+                throw new Error ('Id deve ser string.')
+            }
+            if(newId.length < 2){
+                res.status(400)
+                throw new Error ('Id deve possuir no mínimo 2 caracteres.')
+            }
+        }
+        
+        if(newName !== undefined){
+            if(typeof newName !== "string"){
+                res.status(400)
+                throw new Error ('Name deve ser string.')
+            }
+            if(newName.length < 3){
+                res.status(400)
+                throw new Error ('Name deve possuir no mínimo 3 caracteres.')
+            }
+        }
+        
+        const [band] = await db.raw(`
+            SELECT * FROM bands
+            WHERE id = "${id}";
+        `)
+
+        if(band){
+            await db.raw(`
+            UPDATE bands
+            SET 
+                id = "${newId || band.id}", 
+                name = "${newName || band.name}"
+            WHERE id = "${id}";
+        `)
+        } else {
+            res.status(404)
+            throw new Error ('Id não encontrado.')
+        }
+
+        res.status(200).send('Requisição feita com sucesso.')
+
+    } catch (error){
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+
     }
 })
